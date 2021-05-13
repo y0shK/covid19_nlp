7a403f4e4ff2ecfeaba058f22621cd86d9062cbe
Yash Karandikar <ynkarandikar@gmail.com>
2021-05-13 10:39:49 -0700

    add id to data and put user/rake-nltk keywords in hashTable

--- covid19_nlp.py
+++ covid19_nlp.py
@@ -41,20 +41,51 @@ vaccine_tweets = pd.read_csv("vaccination_all_tweets.csv")
 
 # take a subset of the csv and put it into a dataframe
 # https://www.askpython.com/python/examples/subset-a-dataframe
-march_df = march_tweets_train[["TweetAt", "OriginalTweet", "Sentiment"]]
-march_df.append(march_tweets_test[["TweetAt", "OriginalTweet", "Sentiment"]])
+march_df = march_tweets_train[["UserName", "TweetAt", "OriginalTweet", "Sentiment"]]
+march_df.append(march_tweets_test[["UserName", "TweetAt", "OriginalTweet", "Sentiment"]])
 print(march_df)
 
 july_df = july_tweets[["date", "text", "hashtags"]]
-vaccine_df = vaccine_tweets[["date", "text", "hashtags"]]
+
+# convert user_name to integer
+# start with index 0, 1, ..., n
+# add this to a list
+# and add the list to the df as the "username" column
+
+# https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
+# https://stackoverflow.com/questions/18265935/python-create-list-with-numbers-between-2-values
+id_list = list(range(1, len(july_df) + 1))
+july_df.insert(0, "id", id_list, False)
+
+print(july_df)
+
+vaccine_df = vaccine_tweets[["id", "date", "text", "hashtags"]]
 
 # start by using rake-nltk to find key words
 r = Rake()
-r.extract_keywords_from_text(march_df["OriginalTweet"][1])
-print(r.get_ranked_phrases_with_scores())
 
-# perform sentiment analysis on each using vader
+# also perform sentiment analysis on each using vader
 # https://www.geeksforgeeks.org/python-sentiment-analysis-using-vader/
+
 sid = SentimentIntensityAnalyzer()
-sentiment_dict = sid.polarity_scores(march_df["OriginalTweet"][1])
-print(sentiment_dict)
+
+#for i in range(len(march_df["OriginalTweet"])):
+
+# store the user number (randomly generated) as key and top 3 words by rake-nltk
+# in a dictionary (hashTable)
+
+keyword_dict = {}
+
+for i in range(10):
+    r.extract_keywords_from_text(march_df["OriginalTweet"][i])
+    list_of_tuples = r.get_ranked_phrases_with_scores()
+
+    # store the top three results of each user in a hashTable
+    # this provides quick lookup and convenient storage of data
+    keyword_dict[march_df["UserName"][i]] = list_of_tuples[0:3]
+
+    sentiment_dict = sid.polarity_scores(march_df["OriginalTweet"][i])
+    print(sentiment_dict)
+    print("\n")
+
+print(keyword_dict)
