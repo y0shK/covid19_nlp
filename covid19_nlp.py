ef67bcdb14cd1e1753afff481a4b5c50ee0d4286
Yash Karandikar <ynkarandikar@gmail.com>
2021-05-12 17:46:19 -0700

    form subset of entire csv file

--- covid19_nlp.py
+++ covid19_nlp.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from rake_nltk import Rake
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 
 # general research area: gauging people's reactions to COVID-19 over time
@@ -35,4 +37,24 @@ july_tweets = pd.read_csv("covid19_tweets.csv")
 os.chdir("C:\\Users\\ynkar\\Desktop\\computational_health\\vaccination_tweets_kaggle")
 
 vaccine_tweets = pd.read_csv("vaccination_all_tweets.csv")
-print(vaccine_tweets)
+# print(vaccine_tweets)
+
+# take a subset of the csv and put it into a dataframe
+# https://www.askpython.com/python/examples/subset-a-dataframe
+march_df = march_tweets_train[["TweetAt", "OriginalTweet", "Sentiment"]]
+march_df.append(march_tweets_test[["TweetAt", "OriginalTweet", "Sentiment"]])
+print(march_df)
+
+july_df = july_tweets[["date", "text", "hashtags"]]
+vaccine_df = vaccine_tweets[["date", "text", "hashtags"]]
+
+# start by using rake-nltk to find key words
+r = Rake()
+r.extract_keywords_from_text(march_df["OriginalTweet"][1])
+print(r.get_ranked_phrases_with_scores())
+
+# perform sentiment analysis on each using vader
+# https://www.geeksforgeeks.org/python-sentiment-analysis-using-vader/
+sid = SentimentIntensityAnalyzer()
+sentiment_dict = sid.polarity_scores(march_df["OriginalTweet"][1])
+print(sentiment_dict)
